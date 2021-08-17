c7b398decdaa1a0e63820d07460cbe3342ac27b8
aditipolkam <aditipolkam@gmail.com>
2021-08-17 23:50:27 +0530

    ADDED solution 10 - todo

---
+++ Solutions/10.SummationOfPrimes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int isPrime(int num){
+    for(int i=2; i<=num/2; i++){
+        if(num%i==0)
+            return 0;
+    }
+    return 1;
+}
+int main()
+{
+    double sum = 2;
+    for(double i=3;i<2000000;i=i+2){
+        if(isPrime(i))
+            sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}
